c1bca2374398b9ffe8ed4bca2e670fde83564dfa
kostyacharykov <81875410+kostyacharykov@users.noreply.github.com>
2021-04-14 19:03:24 +0300

    Add Buttons
    Сделал 2 кнопки: "Добавить в начало" и "Добавить в конец"

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-let button = document.querySelector('.add-field__add');
+let button1 = document.querySelector('.add-field__addbegin');
+let button0 = document.querySelector('.add-field__addend');
 let textadd = document.querySelector('.add-field__exercise');
 let area = document.querySelector('.area__half-area');
 
-let addtask = function () {
+let addtask = function (rty) {
   if (textadd.value != '') { // Условие, при котором значение текста в поле для добавления должно быть не равно пустоте
-    area.insertAdjacentHTML('beforeend',`<div class="area__element">
+    area.insertAdjacentHTML(rty,`<div class="area__element">
     <input type="checkbox" class="area__element__check">
     <p class="area__element__text1">${textadd.value}</p>
     <a class='area__element__delete'><img src="img/trash 1.svg" class="area__element__trash"></a>
@@ -13,8 +14,12 @@ let addtask = function () {
   };
 };
 
-button.addEventListener('click', function() {
-  addtask();
+button0.addEventListener('click', function() {
+    addtask(rty = 'beforeend');
+});
+
+button1.addEventListener('click', function() {
+  addtask(rty = 'afterbegin');
 });
 
 textadd.addEventListener('keydown', function (params) {
