6d5ffd26ce8a873ce5a1f84468fabb5ced6b3aad
kostyacharykov <81875410+kostyacharykov@users.noreply.github.com>
2021-04-15 12:39:47 +0300

    Опять четность
    Теперь заменяет, а не добавляет

--- js/main-2.js
+++ js/main-2.js
@@ -4,14 +4,14 @@ let area = document.querySelector('.area__half-area');
 
 let addtask = function () {
   if (textadd.value & 1) {
-    area.insertAdjacentHTML('beforeend',`<div class="area__element">
+    area.innerHTML = (`<div class="area__element">
     <input type="checkbox" class="area__element__check">
     <p class="area__element__text1">Число нечётное</p>
     <a class='area__element__delete'><img src="img/trash 1.svg" class="area__element__trash"></a>
     </div>`); // Добавляем элемент в конец списка area, 'beforeend' - аргумент, передающий позицию.
     textadd.value = ''; // Удалить текст в поле textadd, в нашем случае конкретно после нажатия кнопки
   } else {
-    area.insertAdjacentHTML('beforeend',`<div class="area__element">
+    area.innerHTML = (`<div class="area__element">
     <input type="checkbox" class="area__element__check">
     <p class="area__element__text1">Число чётное</p>
     <a class='area__element__delete'><img src="img/trash 1.svg" class="area__element__trash"></a>
