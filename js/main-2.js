d351248642f0048aef7d41a2412d14d3d034ff40
kostyacharykov <81875410+kostyacharykov@users.noreply.github.com>
2021-04-15 13:00:14 +0300

    ЗП посчитал

--- js/main-2.js
+++ js/main-2.js
@@ -3,32 +3,38 @@ let textadd = document.querySelector('.add-field__exercise');
 let area = document.querySelector('.area__half-area');
 
 let addtask = function () {
-  if (textadd.value & 1) {
-    area.innerHTML = (`<div class="area__element">
-    <input type="checkbox" class="area__element__check">
-    <p class="area__element__text1">Число нечётное</p>
-    <a class='area__element__delete'><img src="img/trash 1.svg" class="area__element__trash"></a>
-    </div>`); // Добавляем элемент в конец списка area, 'beforeend' - аргумент, передающий позицию.
-    textadd.value = ''; // Удалить текст в поле textadd, в нашем случае конкретно после нажатия кнопки
-  } else {
-    area.innerHTML = (`<div class="area__element">
-    <input type="checkbox" class="area__element__check">
-    <p class="area__element__text1">Число чётное</p>
-    <a class='area__element__delete'><img src="img/trash 1.svg" class="area__element__trash"></a>
-    </div>`); // Добавляем элемент в конец списка area, 'beforeend' - аргумент, передающий позицию.
-    textadd.value = ''; 
-  }
+  // textadd.value = textadd.value;
+  area.innerHTML = `<div class="area__element">
+   <input type="checkbox" class="area__element__check">
+   <p class="area__element__text1">${textadd.value * 12}</p>
+   <a class='area__element__delete'><img src="img/trash 1.svg" class="area__element__trash"></a>
+   </div>`;
+  // if (textadd.value & 1) {
+  //   area.innerHTML = (`<div class="area__element">
+  //   <input type="checkbox" class="area__element__check">
+  //   <p class="area__element__text1">Число нечётное</p>
+  //   <a class='area__element__delete'><img src="img/trash 1.svg" class="area__element__trash"></a>
+  //   </div>`); // Добавляем элемент в конец списка area, 'beforeend' - аргумент, передающий позицию.
+  //   textadd.value = ''; // Удалить текст в поле textadd, в нашем случае конкретно после нажатия кнопки
+  // } else {
+  //   area.innerHTML = (`<div class="area__element">
+  //   <input type="checkbox" class="area__element__check">
+  //   <p class="area__element__text1">Число чётное</p>
+  //   <a class='area__element__delete'><img src="img/trash 1.svg" class="area__element__trash"></a>
+  //   </div>`); // Добавляем элемент в конец списка area, 'beforeend' - аргумент, передающий позицию.
+  //   textadd.value = ''; 
+  // }
 };
 
-button0.addEventListener('click', function() {
+textadd.addEventListener('input', function() {
     addtask();
 });
 
-textadd.addEventListener('keydown', function (params) {
-  if (params.key == 'Enter') { 
-    addtask();
-  };
-});
+// textadd.addEventListener('keydown', function (params) {
+//   if (params.key == 'Enter') { 
+//     addtask();
+//   };
+// });
 
 // button.addEventListener('click', function() {
 //   if (textadd.value != '') { // Условие, при котором значение текста в поле для добавления должно быть не равно пустоте
