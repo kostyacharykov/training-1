ce8a2773f4c54adb17a2efcb822f8ac052c43e27
kostyacharykov <81875410+kostyacharykov@users.noreply.github.com>
2021-04-15 14:54:43 +0300

    Добавил строку для внесения количества месяцев

--- js/main-2.js
+++ js/main-2.js
@@ -1,5 +1,6 @@
 let button0 = document.querySelector('.add-field__addend');
 let textadd = document.querySelector('.add-field__exercise');
+let textadd1 = document.querySelector('.add-field__month');
 let area = document.querySelector('.area__half-area');
 
 let eba = function (zp) {
@@ -11,18 +12,19 @@ let eba = function (zp) {
 };
 
 let addtask = function () {
-  if (textadd.value * 12 < 100000) {
-  area.innerHTML = eba;
+  if (textadd.value * textadd1.value < 100000) {
   eba('СЛИШКОМ МАЛО РУБЛЕЙ БЛЯ, ЛОХ ЕБАНЫЙ');
-  } else if (textadd.value * 12 > 1000000) {
-    area.innerHTML = eba;
+  } else if (textadd.value * textadd1.value > 10000000) {
   eba('ТЫ ШО ОХУЕЛ БЛЯ РУБЛЕЙ');
   } else { 
-    area.innerHTML = eba;
-    eba(textadd.value * 12 + ' рублей');  
+    eba(textadd.value * textadd1.value + ' рублей');  
   }
 };
 
 textadd.addEventListener('input', function() {
+    addtask();
+});
+
+textadd1.addEventListener('input', function() {
     addtask();
 });
