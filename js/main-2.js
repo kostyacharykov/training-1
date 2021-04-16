4a3d89f2d3d7669a356fe66bfa715147cf3a98a6
kostyacharykov <81875410+kostyacharykov@users.noreply.github.com>
2021-04-16 08:49:31 +0300

    Добавил разные картинки
    Картинки к градации ЗП

--- js/main-2.js
+++ js/main-2.js
@@ -3,21 +3,28 @@ let textadd = document.querySelector('.add-field__exercise');
 let textadd1 = document.querySelector('.add-field__month');
 let area = document.querySelector('.area__half-area');
 
-let eba = function (zp) {
-  area.innerHTML = `<div class="area__element">
+// const imagefunc = [
+//   img1 = 'img/piggy-bank.svg',
+//   img2 = 'img/save-money.svg',
+//   img3 = 'img/money.svg',
+// ];
+
+let eba = function (zp, img) {
+   return `<div class="area__element">
    <input type="checkbox" class="area__element__check">
    <p class="area__element__text1">${zp}</p>
-   <a class='area__element__delete'><img src="img/trash 1.svg" class="area__element__trash"></a>
+   <a class='area__element__delete'><img src="${img}" class="area__element__trash"></a>
    </div>`;
-};
+  };
 
-let addtask = function () {
-  if (textadd.value * textadd1.value < 100000) {
-  eba('СЛИШКОМ МАЛО РУБЛЕЙ БЛЯ, ЛОХ ЕБАНЫЙ');
-  } else if (textadd.value * textadd1.value > 10000000) {
-  eba('ТЫ ШО ОХУЕЛ БЛЯ РУБЛЕЙ');
+let addtask = function (rer) {
+  rer = textadd.value * textadd1.value;
+  if (rer < 100000) {
+    area.innerHTML = eba('СЛИШКОМ МАЛО РУБЛЕЙ БЛЯ, ЛОХ ЕБАНЫЙ', 'img/piggy-bank.svg');
+  } else if (rer > 10000000) {
+    area.innerHTML = eba('ТЫ ШО ОХУЕЛ БЛЯ РУБЛЕЙ', 'img/money.svg');
   } else { 
-    eba(textadd.value * textadd1.value + ' рублей');  
+    area.innerHTML = eba(rer + ' рублей', 'img/save-money.svg');  
   }
 };
 
