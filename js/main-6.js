5a7103949cf0387d8ac8057c74d192ab01a0cd54
kostyacharykov <81875410+kostyacharykov@users.noreply.github.com>
2021-04-22 12:56:38 +0300

    Убрал forEach

--- js/main-6.js
+++ js/main-6.js
@@ -2,17 +2,11 @@ let grid = document.querySelector('.grid');
 let grid__element__number = document.querySelector('.grid__element__number');
 let grid__element = document.querySelector('.grid__element');
 
-const array = [];
-
 for (let i = 0; i < 50; i++) {
-  array[i] = i + 1;
-};
-
-array.forEach(function (i) {
   let numberarr = `<p class="grid__element__number">${i}</p>`;
-  let grid__element = document.createElement('grid__element');
+  let grid__element = document.createElement('div');
   grid__element.classList.add('grid__element');
   grid__element.innerHTML = numberarr;
 
   grid.append(grid__element);
-});
+};
