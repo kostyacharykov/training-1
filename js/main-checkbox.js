290fbfbaf40a99ce3a1a3f369c9e3d284a09410b
kostyacharykov <81875410+kostyacharykov@users.noreply.github.com>
2021-05-09 11:46:50 +0300

    Сделал 2 чекбокса

--- js/main-checkbox.js
+++ js/main-checkbox.js
@@ -35,6 +35,7 @@ let input2 = document.querySelector('.otpusk');
 let obolochka = document.querySelector('.obolochka');
 
 // ---------------------------------------------------------------------------------
+
 let rer = function (input) {
 let stringname2 = '';
 staff.forEach(function(item) {
@@ -63,70 +64,21 @@ document.querySelector('.obolochka').innerHTML = stringname2;
 };
 
 document.querySelector('.rabota').addEventListener('change', function () {
-  if (input1.checked) {
-rer(input1);
+if (input1.checked) {
+  input2.checked = false;
+  rer(input1);
+  obolochka.style.display = 'block';
   } else {
-    let stringname2 = '';
-staff.forEach(function(item) {
-      stringname2 += `<div class="worker_parameters ${item.onVacation ? 'worker_parameters__onVacation' : ''}">
-      <div class="worker_parameters__names_position">
-      <h4 class="names">${item.name}</h4>
-      <p class="position">${item.position}</p>
-      </div>
-      ${item.onVacation
-        ? `<div class='onVacation_show'>В отпуске</div>`
-        : ''
-      }
-      <p class="age">Возраст: ${item.age} лет</p>
-      <p class="salary">Зарплата: ${item.salary} /
-      месяц</p>
-      </div>
-      `;
-document.querySelector('.obolochka').innerHTML = stringname2;
-});
+    obolochka.style.display = 'none';
   }
 });
 
 document.querySelector('.otpusk').addEventListener('change', function () {
-  if (input2.checked) {
-    rer(input2);
-      } else {
-        let stringname2 = '';
-    staff.forEach(function(item) {
-          stringname2 += `<div class="worker_parameters ${item.onVacation ? 'worker_parameters__onVacation' : ''}">
-          <div class="worker_parameters__names_position">
-          <h4 class="names">${item.name}</h4>
-          <p class="position">${item.position}</p>
-          </div>
-          ${item.onVacation
-            ? `<div class='onVacation_show'>В отпуске</div>`
-            : ''
-          }
-          <p class="age">Возраст: ${item.age} лет</p>
-          <p class="salary">Зарплата: ${item.salary} /
-          месяц</p>
-          </div>
-          `;
-    document.querySelector('.obolochka').innerHTML = stringname2;
-    });
-      }
-    });
-
-let stringname2 = '';
-staff.forEach(function(item) {
-      stringname2 += `<div class="worker_parameters ${item.onVacation ? 'worker_parameters__onVacation' : ''}">
-      <div class="worker_parameters__names_position">
-      <h4 class="names">${item.name}</h4>
-      <p class="position">${item.position}</p>
-      </div>
-      ${item.onVacation
-        ? `<div class='onVacation_show'>В отпуске</div>`
-        : ''
-      }
-      <p class="age">Возраст: ${item.age} лет</p>
-      <p class="salary">Зарплата: ${item.salary} /
-      месяц</p>
-      </div>
-      `;
-document.querySelector('.obolochka').innerHTML = stringname2;
+if (input2.checked) {
+  input1.checked = false;
+  rer(input2);
+  obolochka.style.display = 'block';
+  } else {
+    obolochka.style.display = 'none';
+  }
 });
