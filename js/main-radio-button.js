76a7e66a2ac1cd7ee9bfd4234a4c344d97dce494
kostyacharykov <81875410+kostyacharykov@users.noreply.github.com>
2021-05-10 11:13:41 +0300

    Радио нормально работает

--- js/main-radio-button.js
+++ js/main-radio-button.js
@@ -1,4 +1,3 @@
-
 let block = document.querySelector('.block');
 let input_red = document.querySelector('.input_red');
 let input_green = document.querySelector('.input_green');
@@ -6,25 +5,17 @@ let input_little = document.querySelector('.input_little');
 let input_big = document.querySelector('.input_big');
 
 // ---------------------------------------------------------------------------------
-let wer = function (input, property) {
-  input.addEventListener('click', function () {
-    block.classList.toggle(property);
+let change_property = function (input, property, property_del) {
+  input.addEventListener('change', function () {
+    if (input.checked) {
+      block.classList.remove(property_del);
+      block.classList.add(property);
+    };
+    console.log(block.classList);
   });
 };
 
-wer(input_red, 'color_red');
-wer(input_green, 'color_green');
-wer(input_little, 'size_big');
-wer(input_big, 'size_little');
-
-// input_green.addEventListener('click', function () {
-//   block.classList.toggle('color_green');
-// });
-
-// input_little.addEventListener('click', function () {
-//   block.classList.toggle('size_big');
-// });
-
-// input_big.addEventListener('click', function () {
-//   block.classList.toggle('size_little');
-// });
+change_property(input_red, 'color_red', 'color_green');
+change_property(input_green, 'color_green', 'color_red');
+change_property(input_little, 'size_big', 'size_little');
+change_property(input_big, 'size_little', 'size_big');
